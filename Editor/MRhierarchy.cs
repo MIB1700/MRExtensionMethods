a24e5081bd514cc648ef103d8b4ae8c704929a9f
Martin Ritter <signup@martin-ritter.com>
2021-07-18 00:35:32 +0200

    simplify type recognition a bit and general cleanup

--- Editor/MRhierarchy.cs
+++ Editor/MRhierarchy.cs
@@ -1,9 +1,7 @@
-using System.Diagnostics;
 using UnityEngine;
 using UnityEditor;
 using System;
-// using UnityEditorInternal;
-using System.IO;
+
 
 namespace MR.CustomExtensions
 {
@@ -11,21 +9,20 @@ namespace MR.CustomExtensions
     public class MRhierarchy : /*MonoBehaviour*/ Editor
     {
         static Texture2D texturedMR;
-        // static string editorPath = "";
-        // static string editorGUIPath = "";
+
         static bool isInited = false;
 
-        public static UnityEngine.Color bgC = UnityEngine.Color.black;
-        public static UnityEngine.Color borderC = UnityEngine.Color.white;
-        public static UnityEngine.Color textC = UnityEngine.Color.red;
+        //types we want to be able to use
+        //TODO: would be nice if we could say here what types they are or if they are switches
+        static private string[] types= {"bg:", "b:", "t:", "bs:", "ts:"};
 
         static MRhierarchy()
         {
             EditorApplication.hierarchyWindowItemOnGUI -= HierarchyWindowItemOnGUI;
             EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItemOnGUI;
-            Initialize();
         }
 
+        //initialize the png asset...
         static void Initialize()
         {
             if (isInited)
@@ -33,21 +30,18 @@ namespace MR.CustomExtensions
                 return;
             }
 
-            string[] guids2 = AssetDatabase.FindAssets("MR_icon_blue_15x15 t:texture2D");
+            string[] guids2 = AssetDatabase.FindAssets("MR_icon_blue_32x32 t:texture2D");
 
             if (guids2.Length >= 1)
             {
-
                 var pathToPng = AssetDatabase.GUIDToAssetPath(guids2[0]);
-
                 texturedMR = (Texture2D)AssetDatabase.LoadAssetAtPath<Texture2D>(pathToPng);
-
-                // Debug.Log(texturedMR);
             }
             else
             {
-                UnityEngine.Debug.Log($"MR_icon_blue_32x32.png NOT FOUND...");
+                UnityEngine.Debug.LogError($"MR_icon_blue_32x32.png NOT FOUND...");
             }
+
             isInited = true;
         }
 
@@ -60,175 +54,142 @@ namespace MR.CustomExtensions
                 return;
             }
 
+            Rect BackgroundRect = new Rect();
+
             //if the name of the GO starts with a double "//" i.e. like a comment...
             if (gameObject.name.StartsWith("//", System.StringComparison.Ordinal))
             {
-                float xPos = selectionRect.position.x + 60f - 28f - selectionRect.xMin;
-                float yPos = selectionRect.position.y;
+                Color backGroundColour  = UnityEngine.Color.black;
+                Color borderColour      = UnityEngine.Color.white;
+                Color textColour        = UnityEngine.Color.red;
+
+                float textSize = 12f;
+                float borderSize = 2f;
+
+                float xPos  = selectionRect.position.x + 60f - 28f - selectionRect.xMin;
+                float yPos  = selectionRect.position.y;
                 float xSize = selectionRect.size.x + selectionRect.xMin + 28f - 60 + 16f;
                 float ySize = selectionRect.size.y;
 
-                Rect BackgroundRect = new Rect(xPos, yPos, xSize, ySize);
+                BackgroundRect = new Rect(xPos, yPos, xSize, ySize);
 
-                // selectionRect.xMin -= 26;
-                // selectionRect.xMax += 16;
-
-                var offset = BackgroundRect;
-
+                var offset = BackgroundRect.Shrink(borderSize);
                 gameObject.SetActive(false);
-                // EditorGUI.DrawRect(selectionRect, borderC);
 
-                var name = gameObject.name;
+                //make sure "/" is removed even if no types are changed!
+                var name = gameObject.name.Replace("/", "");
 
-                (UnityEngine.Color backGroundColour, string finalName)   = GetColorFromString(name, "bg:");
-
-                name = finalName ?? name;
-                (UnityEngine.Color textColour, string finalTextName)     = GetColorFromString(name, "t:");
-
-                name = finalTextName ?? name;
-                (UnityEngine.Color borderColour, string finalBorderName) = GetColorFromString(name, "b:");
-
-
-                name = finalBorderName ?? name;
-                (float bOffset, string offsetName) = GetNumberFromString(name, "bs:", 2);
-
-                name = offsetName ?? name;
-                (float textSize, string textSizeName) = GetNumberFromString(name, "ts:", 12);
-
-                name = textSizeName ?? name;
-                name = name.Replace("/", "");
-
-                offset = BackgroundRect.Shrink(bOffset);
-
-                if (finalBorderName != null)
+                //loop through array of known types and do magic...
+                foreach (var type in types)
                 {
-                    EditorGUI.DrawRect(BackgroundRect, borderColour);
-                }
-                else
-                {
-                    EditorGUI.DrawRect(BackgroundRect, borderC);
-                }
+                    (string after, string finalName) = GetStringAfterType(name, type);
 
-                if (finalName != null)
-                {
-                    EditorGUI.DrawRect(offset, backGroundColour);
-                }
-                else
-                {
-                    EditorGUI.DrawRect(offset, bgC);
+                    if (after == null) {
+                        //if that type wasn't found in string => bail
+                        continue;
+                    }
+
+                    //only use the "finalName" if it isn't null
+                    name = finalName ?? name;
+
+                    //deal with the types
+                    switch (type)
+                    {
+                        case "bg:":
+                            backGroundColour = ConvertStringToColor(after, Color.white);
+                            break;
+                        case "b:":
+                            borderColour = ConvertStringToColor(after, Color.white);
+                            break;
+                        case "t:":
+                            textColour = ConvertStringToColor(after, Color.white);
+                            break;
+                        case "bs:":
+                            offset = BackgroundRect.Shrink(ConvertStringToFloat(after, 2));
+                            break;
+                        case "ts:":
+                            textSize = ConvertStringToFloat(after, 12);
+                            break;
+                    }
                 }
 
-                if (finalTextName != null)
+                //all variables are set, now draw and put the text
+                EditorGUI.DrawRect(BackgroundRect, borderColour);
+                EditorGUI.DrawRect(offset, backGroundColour);
+
+                EditorGUI.LabelField(BackgroundRect, name, new GUIStyle()
                 {
-                    EditorGUI.LabelField(BackgroundRect, name, new GUIStyle()
-                        {
-                            normal = new GUIStyleState() { textColor = textColour },
-                            fontStyle = FontStyle.BoldAndItalic,
-                            fontSize = (int)textSize,
-                            wordWrap = true,
-                            alignment = TextAnchor.MiddleCenter
-                        }
-                    );
-                }
-                else
-                {
-                    EditorGUI.LabelField(BackgroundRect, name, new GUIStyle()
-                        {
-                            normal = new GUIStyleState() { textColor = textC },
-                            fontStyle = FontStyle.BoldAndItalic,
-                            fontSize = (int)textSize,
-                            wordWrap = true,
-                            alignment = TextAnchor.MiddleCenter
-                        }
-                    );
-                }
+                        normal = new GUIStyleState() { textColor = textColour },
+                        fontStyle = FontStyle.BoldAndItalic,
+                        fontSize = (int)textSize,
+                        wordWrap = true,
+                        alignment = TextAnchor.MiddleCenter
+                    }
+                );
             }
 
-            if (gameObject.GetComponent(typeof(IMR)))
-            {
-                Rect rect = new Rect(selectionRect.x - 25f, selectionRect.y, selectionRect.height, selectionRect.height);
-                rect.x = selectionRect.x + selectionRect.width - 15f;
+            //TODO: figure out why this does not draw the image... check proper rect location!
+            if (gameObject.GetComponent(typeof(IMR))){
 
-                // Debug.Log($"rect: {rect}");
-                GUI.Label(rect, texturedMR);
-                return;
+                DrawIcon(BackgroundRect, texturedMR);
             }
         }
 
-        //provide the name of the gameobject and the type (e.g. bg, t, b) to look for colour name, also return the
-        //name of the gameobject without the colour name in it
-        private static (UnityEngine.Color color, string finalName) GetColorFromString(string cString, string type)
-        {
-            UnityEngine.Color finalColor = UnityEngine.Color.white;
-            string newCol = "";
+        private static (string withoutType, string final) GetStringAfterType(string cString, string type) {
+
+            string newStr = "";
             string finalName = CheckForWhiteSpaceAfterType(cString, type);
 
             //remove the "//" since we done't want to see this on the GO in the hierarchy
             finalName = finalName.Replace("/", "");
 
             //split string into words, delimited by spaces
-            var cols = finalName.Split(' ');
+            var strgs = finalName.Split(' ');
 
-            //loop through words, checking for the "type" to create the final colour
-            foreach (var col in cols)
+            //loop through words, checking for the "type", return the string after the type and a string
+            //containing the type AND the string after it!
+            foreach (var str in strgs)
             {
-                if (col.Contains(type))
+                if (str.Contains(type))
                 {
                     //remove any of the filler characters
-                    newCol = col.Replace("_", "").Replace("-", "").Replace(type, "").Replace(" ", "");
+                    newStr = str.Replace("_", "").Replace("-", "").Replace(type, "").Replace(" ", "");
+                    finalName = finalName.Replace(str, "");
 
-                    finalName = finalName.Replace(col, "");
-
-                    // //convert string colour name to actual Unity Color...
-                    if (ColorUtility.TryParseHtmlString(newCol, out finalColor))
-                    {
-                        //  UnityEngine.Debug.Log($"type: {type} col: {newCol}");
-                        return (finalColor, finalName);
-                    }
-                    else
-                    {
-                        UnityEngine.Debug.LogError($"Color: {newCol} for Type: {type} could not be converted...");
-                        return (UnityEngine.Color.white, finalName);
-                    }
+                    return (newStr, finalName);
                 }
             }
-
-            return (finalColor, null); //Color cannot be null => return null for the string so we can test if
-                                       //function was succesful or not
+            return (null, null);
         }
 
-        private static (float borderSize, string finalName) GetNumberFromString(string cString, string type, float def)
+        //provide the name of the gameobject and the type (e.g. bg, t, b) to look for colour name, also return the
+        //name of the gameobject without the colour name in it
+
+        //try to convert the string to a Unity Color...
+        private static Color ConvertStringToColor(string noType, Color defCol)
         {
-            float offset = def;
-            string newCol = "";
-            string finalName = "";
-            string nameCheck = CheckForWhiteSpaceAfterType(cString, type);
+            if (noType == null) {
 
-            //remove the "//" since we done't want to see this on the GO in the hierarchy
-            finalName = nameCheck.Replace("/", "");
-
-            //split string into words, delimited by spaces
-            var cols = finalName.Split(' ');
-
-            //loop through words, checking for the "type" to create the final colour
-            foreach (var col in cols)
-            {
-                if (col.Contains(type))
-                {
-                    //remove any of the filler characters
-                    newCol = col.Replace("_", "").Replace("-", "").Replace(type, "");
-
-                    finalName = finalName.Replace(newCol, "").Replace(type, "");
-
-                    if (Single.TryParse(newCol, out offset))
-                    {
-                        return(offset, finalName);
-                    }
-
-                    return(def, finalName);
-                }
+                return defCol;
             }
-            return (def, null);
+            if (ColorUtility.TryParseHtmlString(noType, out Color finalColor))
+            {
+                return finalColor;
+            }
+            else
+            {
+                UnityEngine.Debug.LogError($"Color: {noType} could not be converted...");
+                return defCol;
+            }
+        }
+
+        private static float ConvertStringToFloat(string noType, float def) {
+
+            if (Single.TryParse(noType, out float offset))
+            {
+                return offset;
+            }
+            return def;
         }
 
         private static string CheckForWhiteSpaceAfterType(string cString, string type)
@@ -261,8 +222,19 @@ namespace MR.CustomExtensions
                     }
                 }
             }
-
             return nameCheck;
+        }
+
+
+        //this is for drawing the image... not useful yet...
+        private static void DrawIcon(Rect selectionRect, Texture2D icon) {
+
+            //look for assets only if we are actually trying to use them...
+            Initialize();
+            selectionRect.x = selectionRect.x + selectionRect.width - 15f;
+
+                // Debug.Log($"rect: {rect}");
+            GUI.Label(selectionRect, icon);
         }
     }
 }
--- Editor/MRhierarchyWindow.cs
+++ Editor/MRhierarchyWindow.cs
@@ -1,16 +1,18 @@
-using UnityEditor;
-using MR.CustomExtensions;
-public class MRhierarchyWindow : EditorWindow
-{
-    [MenuItem("Tools/Hierarchy Editor")]
-    public static void ShowWindow()
-    {
-        GetWindow<MRhierarchyWindow>("HierarchyEditor");
-    }
-    private void OnGUI()
-    {
-        MRhierarchy.bgC = EditorGUILayout.ColorField("Background Colour", MRhierarchy.bgC);
-        MRhierarchy.borderC = EditorGUILayout.ColorField("Border Colour", MRhierarchy.borderC);
-        MRhierarchy.textC = EditorGUILayout.ColorField("Text Colour", MRhierarchy.textC);
-    }
-}
+// using UnityEditor;
+// using MR.CustomExtensions;
+// public class MRhierarchyWindow : EditorWindow
+// {
+//     [MenuItem("Tools/Hierarchy Editor")]
+//     public static void ShowWindow()
+//     {
+//         GetWindow<MRhierarchyWindow>("HierarchyEditor");
+//     }
+//     private void OnGUI()
+//     {
+//         MRhierarchy.backGroundColour = EditorGUILayout.ColorField("Background Colour", MRhierarchy.backGroundColour);
+//         MRhierarchy.borderColour = EditorGUILayout.ColorField("Border Colour", MRhierarchy.borderColour);
+//         MRhierarchy.textColour = EditorGUILayout.ColorField("Text Colour", MRhierarchy.textColour);
+//         MRhierarchy.textSize = EditorGUILayout.FloatField("Text Size: ", MRhierarchy.textSize);
+//         MRhierarchy.borderSize = EditorGUILayout.FloatField("Text Size: ", MRhierarchy.borderSize);
+//     }
+// }
